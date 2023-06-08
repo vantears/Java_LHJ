900196452d34a3a8ecc9dc5ab6b005ee00e02c40
Horiring <124111575+Horiring@users.noreply.github.com>
2023-06-08 10:20:40 +0900

    7일차

--- java/src/day7/continueEx/EvenNumberEx.java
+++ java/src/day7/continueEx/EvenNumberEx.java
@@ -3,7 +3,13 @@ package day7.continueEx;
 public class EvenNumberEx {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				continue;
+			}
+			System.out.println(i);
+		}
 
 	}
 
