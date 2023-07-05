bd94b1a486c9d20a0c36151755ce08814725ad66
Horiring <124111575+Horiring@users.noreply.github.com>
2023-07-05 15:15:35 +0900

    25일차

--- java/src/day25/practice/Controller.java
+++ java/src/day25/practice/Controller.java
@@ -1,5 +1,12 @@
 package day25.practice;
 
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -13,11 +20,14 @@ public class Controller {
 	
 	
 	public void run() {
+		String fileName = "src/day25/practice/student.txt";
+		load(fileName);
 		do {
 			menuPrint();
 			menu = sc.nextInt();
 			optionPrint(menu);
 		} while(menu != EXIT);
+		save(fileName);
 		sc.close();
 		
 	}
@@ -38,7 +48,7 @@ public class Controller {
 			addStudent();
 			break;
 		case 2:
-			searchStudent();
+			searchMenu();
 			break;
 		case 3:
 			System.out.println("Quitting!");
@@ -48,6 +58,39 @@ public class Controller {
 		}
 		
 	}
+	
+	private void save(String fileName) {
+		try(FileOutputStream fos = new FileOutputStream(fileName);
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			FileOutputStream count = new FileOutputStream("src/day25/practice/count.txt");){
+				for(Student tmp : list) {
+					oos.writeObject(tmp);
+				}
+				count.write(listCount);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void load(String fileName) {
+		try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream(fileName));
+			FileInputStream sCount = new FileInputStream("src/day25/practice/count.txt");) {
+			int loadList = sCount.read();
+			listCount = loadList;
+			while(true) {
+				Student tmp = (Student)ois.readObject();
+				list.add(tmp);
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("No file to load!");
+		} catch (EOFException e) {
+			System.out.println("File loaded!");
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			System.out.println("No Student class is found!");
+		}
+	}
 
 	private void addStudent() {
 		sc.nextLine();
@@ -55,14 +98,36 @@ public class Controller {
 		String major = sc.nextLine();
 		System.out.print("Enter the student's name : ");
 		String name = sc.nextLine();
-		list.add(new Student(major, name));
+		list.add(new Student(major, name, listCount));
 		listCount++;
-		System.out.println("Registered student ID : " + list.get(listCount - 1).getNum());
-		System.out.println("Registered student Major : " + list.get(listCount - 1).getMajor());
-		System.out.println("Registered student Name : " + list.get(listCount - 1).getName());
+		System.out.println("Successfully registered!");
 		
 		
 		
+	}
+	
+	private void searchMenu() {
+		System.out.println("1. Check all the students");
+		System.out.println("2. Search a student");
+		System.out.print("Choose an option : ");
+		int searchMenu = sc.nextInt();
+		switch(searchMenu) {
+		case 1:
+			printAllStudent();
+			break;
+		case 2:
+			searchStudent();
+			break;
+		default:
+			System.out.println("Wrong option!");
+		}
+		
+	}
+	
+	private void printAllStudent() {
+		for(Student tmp : list) {
+			tmp.print();
+		}
 	}
 	
 	private void searchStudent() {
@@ -70,9 +135,7 @@ public class Controller {
 		String num = sc.next();
 		for(int i = 0; i < list.size(); i++) {
 			if(list.get(i).getNum().equals(num)) {
-				System.out.println("Student ID : " + list.get(i).getNum());
-				System.out.println("Student Major : " + list.get(i).getMajor());
-				System.out.println("Student Name : " + list.get(i).getName());
+				list.get(i).print();
 				return;
 			}
 		}
--- java/src/day25/practice/Student.java
+++ java/src/day25/practice/Student.java
@@ -1,30 +1,39 @@
 package day25.practice;
 
+import java.io.Serializable;
+
 import lombok.Data;
 
 @Data
-public class Student {
-	private String num;
-	private String major;
-	private String name;
+public class Student implements Serializable {
+
+	private static final long serialVersionUID = -1740341550133479720L;
 	
-	private static int count = 0;
+	private String num, major, name;
+	
 	private static int MAX_SIZE = 2;
 	
 	
 	
-	public Student(String major, String name) {
+	public Student(String major, String name, int listCount) {
 		this.major = major;
 		this.name = name;
-		creatNum();
+		creatNum(listCount);
 	}
 	
-	private void creatNum() {
-		String num = "" + ++count;
+	private void creatNum(int listCount) {
+		String num = "" + ++listCount;
 		for(int i = 0; i < MAX_SIZE - num.length(); i++) {
 			num = "0" + num;
 		}
 		this.num = "2023" + num;
+	}
+	
+	public void print() {
+		System.out.println("----------");
+		System.out.println("Student ID : " + num);
+		System.out.println("Major : " + major);
+		System.out.println("Name : " + name);
 	}
 	
 	
