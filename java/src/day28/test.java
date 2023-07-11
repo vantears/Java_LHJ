77cef6dc253866656e3e063222ac3a0c917e705c
Horiring <124111575+Horiring@users.noreply.github.com>
2023-07-11 09:08:53 +0900

    28일차

---
+++ java/src/day28/test.java
@@ -0,0 +1,18 @@
+package day28;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+import lombok.Data;
+
+public class test {
+
+	public static void main(String[] args) {
+		
+		}
+}
+
+@Data
+class Food implements Serializable{
+	
+}
